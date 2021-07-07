b5c418f51199566663c9f338ddff511b48f18319
TaraMKelly <tara.kelly16@gmail.com>
2021-07-07 10:00:40 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  document.querySelector("#create-task-form").addEventListener("submit", e => {
+    e.preventDefault()
+    const ul = document.querySelector("ul#tasks")
+    let li = document.createElement("li")
+    ul.append(li)
+    newItem = e.target["new-task-description"].value
+    console.log(newItem)
+    li = newItem
+    return ul.append(newItem)
+  })
+
+
